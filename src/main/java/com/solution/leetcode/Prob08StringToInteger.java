d14a3dabbb09b5ba44e9ac5513d6cad3cc9bff8b
Thong <me@example.com>
2020-08-20 04:18:32 -0500

    Additional algorithm improvements

--- src/main/java/com/solution/leetcode/Prob08StringToInteger.java
+++ src/main/java/com/solution/leetcode/Prob08StringToInteger.java
@@ -1,11 +1,93 @@
 package com.solution.leetcode;
 
+import static java.lang.Integer.MIN_VALUE;
+import static java.lang.Integer.MAX_VALUE;
+
 /**
  * Created by squall on 8/9/20.
  */
 public class Prob08StringToInteger {
+  boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  boolean isPlusSign(char c) {
+    return c == '+';
+  }
+
+  boolean isMinusSign(char c) {
+    return c == '-';
+  }
+
+  boolean isNumericSign(char c) {
+    return isPlusSign(c) || isMinusSign(c);
+  }
+
+  boolean isSpace(char c) {
+    return c == ' ';
+  }
+
+  boolean isDot(char c) {
+    return c == '.';
+  }
+
+  int getIntValue(char c) {
+    return c - '0';
+  }
+
+  boolean isValidNumeric(char c) {
+    return isDigit(c) || isDot(c);
+  }
+
+  int getFinalResult(long num, int flag) {
+    return num > MAX_VALUE ? (flag > 0 ? MAX_VALUE : MIN_VALUE) : (int)(num * flag);
+  }
 
   public int myAtoi(String str) {
-    return 0;
+    if (str.length() == 0) return 0;
+    if (str.length() == 1 && !isDigit(str.charAt(0))) {
+      return 0;
+    }
+    int start = -1;
+    int flag = 1;
+    long num = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (start < 0) {
+        if (!(isSpace(str.charAt(i))
+          || isNumericSign(str.charAt(i))
+          || isDigit(str.charAt(i)))) {
+          return 0;
+        }
+        if (isDigit(str.charAt(i))) {
+          start = i;
+          num = getIntValue(str.charAt(i));
+        }
+        if (isNumericSign(str.charAt(i)) && !isDigit(str.charAt(i + 1))) {
+          return 0;
+        }
+        if (isMinusSign(str.charAt(i))) {
+          flag = -1;
+        }
+      } else {
+        if (isSpace(str.charAt(i)) || isNumericSign(str.charAt(i))
+          || !(isDigit(str.charAt(i)) || isDot(str.charAt(i)))) {
+          break;
+        }
+        if (isDot(str.charAt(i))) {
+          if (getIntValue(str.charAt(i + 1)) >= 5) {
+            return getFinalResult(num + 1, flag);
+          }
+          else {
+            return getFinalResult(num, flag);
+          }
+        }
+        num = num * 10 + getIntValue(str.charAt(i));
+        int checkResult = getFinalResult(num, flag);
+        if (checkResult == MAX_VALUE || checkResult == MIN_VALUE) {
+          return checkResult;
+        }
+      }
+    }
+    return getFinalResult(num, flag);
   }
 }
--- src/test/java/com/solution/leetcode/Prob08StringToIntegerTest.java
+++ src/test/java/com/solution/leetcode/Prob08StringToIntegerTest.java
@@ -18,7 +18,16 @@ public class Prob08StringToIntegerTest {
 
   @Test
   public void run() {
+    assertEquals(1234, prob8Instance.myAtoi("1234 haha"));
     assertEquals(47, prob8Instance.myAtoi("47"));
     assertEquals(-47, prob8Instance.myAtoi("-47"));
+    assertEquals(1, prob8Instance.myAtoi("+1"));
+    assertEquals(Integer.MIN_VALUE, prob8Instance.myAtoi("-6147483648"));
+    assertEquals(4193, prob8Instance.myAtoi("4193 with words"));
+    assertEquals(25, prob8Instance.myAtoi("25.2"));
+    assertEquals(26, prob8Instance.myAtoi("25.7"));
+    assertEquals(1, prob8Instance.myAtoi("1"));
+    assertEquals(0, prob8Instance.myAtoi("0"));
+    assertEquals(Integer.MAX_VALUE, prob8Instance.myAtoi("9223372036854775808"));
   }
 }
