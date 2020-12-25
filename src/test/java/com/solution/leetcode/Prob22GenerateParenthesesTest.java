670d0e2abb0f4f69e626ec3cda7cd505fadbaacd
Thong Nguyen <nhthong2007@gmail.com>
2020-12-25 01:36:30 -0800

    LC-22 Generate Parentheses (#11)

---
+++ src/test/java/com/solution/leetcode/Prob22GenerateParenthesesTest.java
@@ -0,0 +1,33 @@
+package com.solution.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is unit test for problem 22
+ */
+public class Prob22GenerateParenthesesTest {
+  private Prob22GenerateParentheses solver;
+  private List<String> expectedList = new ArrayList<>();
+  String[] nIsFour = new String[] {"(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"};
+
+  @Before
+  public void setup() {
+    solver = new Prob22GenerateParentheses();
+    Collections.addAll(expectedList, nIsFour);
+  }
+
+  @Test
+  public void perform() {
+    List<String> resultList = solver.generateParenthesis(4);
+    Assert.assertEquals(expectedList.size(), resultList.size());
+    for (String str : expectedList) {
+      Assert.assertTrue(resultList.contains(str));
+    }
+  }
+}
